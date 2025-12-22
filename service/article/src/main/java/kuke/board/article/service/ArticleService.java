2918b290f712a779f7ca64d4be6702492117d9b8
dengo25 <dengo25.dev@gmail.com>
2025-12-22 17:57:08 +0900

    게시글 테스트 데이터 삽입

--- service/article/src/main/java/kuke/board/article/service/ArticleService.java
+++ service/article/src/main/java/kuke/board/article/service/ArticleService.java
@@ -31,6 +31,7 @@ public class ArticleService {
     return ArticleResponse.from(article);
   }
   
+  @Transactional
   public ArticleResponse read(Long articleId) {
     return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
   }
