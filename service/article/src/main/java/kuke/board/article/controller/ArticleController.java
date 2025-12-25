c8258d5b6df3322a2ef63066aa14d5fda7508170
dengo25 <dengo25.dev@gmail.com>
2025-12-25 15:36:43 +0900

    게시글 수 구현

--- service/article/src/main/java/kuke/board/article/controller/ArticleController.java
+++ service/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -51,6 +51,10 @@ public class ArticleController {
   @DeleteMapping("/v1/articles/{articleId}")
   public void delete(@PathVariable Long articleId) {
     articleService.delete(articleId);
-    
+  }
+  
+  @GetMapping("/v1/articles/boards/{boardId}/count")
+  public Long count(@PathVariable Long boardId) {
+    return articleService.count(boardId);
   }
 }
