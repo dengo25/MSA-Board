0e5e298ea0586e8ed521a7f7f89f4800e08834b7
dengo25 <dengo25.dev@gmail.com>
2025-12-25 15:26:11 +0900

    좋아요 수 구현

---
+++ service/like/src/main/java/kuke/board/like/repository/ArticleLikeCountRepository.java
@@ -0,0 +1,34 @@
+package kuke.board.like.repository;
+
+import jakarta.persistence.LockModeType;
+import kuke.board.like.entity.ArticleLikeCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArticleLikeCountRepository extends JpaRepository<ArticleLikeCount, Long> {
+  // select .. for update -> 비관적인 락
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  Optional<ArticleLikeCount> findLockedByArticleId(Long articleId);
+  
+  // 비관적인 락  현재 데이터 기준으로 +1하는 증감 연산
+  @Query(
+      value = "update article_like_count set like_count = like_count + 1 where article_id = :articleId",
+      nativeQuery = true
+  )
+  @Modifying //update쿼리시 필요
+  int increase(@Param("articleId") Long articleId);
+  
+  
+  @Query(
+      value = "update article_like_count set like_count = like_count - 1 where article_id = :articleId",
+      nativeQuery = true
+  )
+  @Modifying
+  int decrease(@Param("articleId") Long articleId);}
